58353e948dbfcb892e74ebe7e0ce23aab8b9b8b2
samarjit.samanta <samarjit.samanta@d445284c-bc84-11de-ad49-1f49b0b801a7>
2009-10-19 18:04:58 +0000

    added properties of job advert DTO

--- trunk/src/dto/JobAdvertDTO.java
+++ trunk/src/dto/JobAdvertDTO.java
@@ -5,15 +5,21 @@ package dto;
 public class JobAdvertDTO implements PropDTO{
 	
 	private int Id=0;
-	private String adDesc;
 	private String cmpnyDesc;
+	private String jobrefcode;
+	private String cmpname;
+	private String department;
+	private String jobtitle;
+	private String jobdesc;
+	private String techskills;
+	private String mgmtskills;
+	private String noyrexp;
+	private String salaryrange;
+	private String startdate;
+	private String location;
+	private String advertizerref;
+	
 
-	public String getAdDesc() {
-		return adDesc;
-	}
-	public void setAdDesc(String adDesc) {
-		this.adDesc = adDesc;
-	}
 	public String getCmpnyDesc() {
 		return cmpnyDesc;
 	}
@@ -29,7 +35,79 @@ public class JobAdvertDTO implements PropDTO{
 	}
 
 	public String toString(){
-		return "\n#id:"+Id+"|desc:"+adDesc+"|cmpnyDesc:"+cmpnyDesc;
+		return "\n#id:"+Id+"|jobrefcode:"+jobrefcode+"| jobtitle:"+jobtitle+"|cmpnyDesc:"+cmpnyDesc+"| advertizerref:"+advertizerref;
 	}
-
+	public String getJobrefcode() {
+		return jobrefcode;
+	}
+	public void setJobrefcode(String jobrefcode) {
+		this.jobrefcode = jobrefcode;
+	}
+	public String getCmpname() {
+		return cmpname;
+	}
+	public void setCmpname(String cmpname) {
+		this.cmpname = cmpname;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getJobtitle() {
+		return jobtitle;
+	}
+	public void setJobtitle(String jobtitle) {
+		this.jobtitle = jobtitle;
+	}
+	public String getJobdesc() {
+		return jobdesc;
+	}
+	public void setJobdesc(String jobdesc) {
+		this.jobdesc = jobdesc;
+	}
+	public String getTechskills() {
+		return techskills;
+	}
+	public void setTechskills(String techskills) {
+		this.techskills = techskills;
+	}
+	public String getMgmtskills() {
+		return mgmtskills;
+	}
+	public void setMgmtskills(String mgmtskills) {
+		this.mgmtskills = mgmtskills;
+	}
+	public String getNoyrexp() {
+		return noyrexp;
+	}
+	public void setNoyrexp(String noyrexp) {
+		this.noyrexp = noyrexp;
+	}
+	public String getSalaryrange() {
+		return salaryrange;
+	}
+	public void setSalaryrange(String salaryrange) {
+		this.salaryrange = salaryrange;
+	}
+	public String getStartdate() {
+		return startdate;
+	}
+	public void setStartdate(String startdate) {
+		this.startdate = startdate;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getAdvertizerref() {
+		return advertizerref;
+	}
+	public void setAdvertizerref(String advertizerref) {
+		this.advertizerref = advertizerref;
+	}
+	
 }
