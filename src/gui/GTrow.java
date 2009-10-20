384f7acb47b3685a906ea6eec1bd69808b7a64af
samarjit.samanta <samarjit.samanta@d445284c-bc84-11de-ad49-1f49b0b801a7>
2009-10-20 06:00:41 +0000

    Added some comments

--- src/gui/GTrow.java
+++ src/gui/GTrow.java
@@ -1,10 +1,23 @@
 package gui;
 
+/**
+* @Author               :       samarjit
+* @Creation Date        :       14/03/2009
+* @Description          :    GTrow implementaion to be used with GTable
+*  
+* @          -------------------------------------------------
+* @Revision:  Revision Date    Name     Change Description      
+* @          -------------------------------------------------     
+* @                      
+*/
+
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Formatter;
+
+
 
 public class GTrow{
 	private int height;
@@ -60,6 +73,11 @@ public class GTrow{
 	public ArrayList<GTcell> getCells() {
 		return cells;
 	}
+	/**
+	 * Constructor for creating the heading row
+	 * @param gt
+	 * @param component
+	 */
 	public GTrow(GTable gt,Panel component){
 		parentElement = gt;
 		y = this.curY = gt.getCurY();
@@ -68,6 +86,13 @@ public class GTrow{
 		cells=new ArrayList<GTcell>();
 		isHedingRow = true;
 	}
+	/**
+	 * Constructor for creating the normal row given the reference of heading row, so that widths of 
+	 * columns can be set properly.
+	 * 
+	 * @param gt
+	 * @param component
+	 */
 	public GTrow(GTable gt,GTrow headingRow, Panel component){
 		parentElement = gt;
 		y = this.curY = gt.getCurY();
@@ -76,12 +101,23 @@ public class GTrow{
 		this.headingRow = headingRow;
 		this.component = component;
 	}
+	/**
+	 * Nothing do here as per current implementation. Is used to give it a uniformity of table creation
+	 * marks table creation start. Future fixed size table implementation may use it to initialize resources. 
+	 */
 	public void trStart(){
 
 	}
 	public void actionHappened(String cmd){
 		//System.out.println("Action happened now"+cmd);
 	}
+	/**
+	 * Heading cell creation takes a parameter width which will become the column width.
+	 * Text is populated inside cells.
+	 * @param width
+	 * @param text
+	 * @throws Exception
+	 */
 	public void addHeadingCell(int width, String text) throws Exception{
 		GTcell tc=new GTcell(this,curX,curY,width,text);
 		this.component.add(tc);
@@ -92,6 +128,11 @@ public class GTrow{
 			}});
 		cells.add(tc);currentCellId++;curX += width;
 	}
+	/**
+	 * Subsequent cell creation, assuming heading cell row has already been created. 
+	 * @param text
+	 * @throws Exception
+	 */
 	public void addCell( String text ) throws Exception{
 		if(headingRow == null){throw new Exception("Bad Table heading reference");}
 		GTcell tc=new GTcell(this,curX,curY,headingRow.cells.get(currentCellId).getWidth(),text);
@@ -106,6 +147,14 @@ public class GTrow{
 		this.component.add(tc);
 
 	}
+	/**
+	 * Called by table at the end of table creation completion.
+	 * This will put in extra cells in a row to make the table look rectangular in shape rather 
+	 * than random pieces of cells strewn over here and there.
+	 * @param rowsize
+	 * @param extraCellTrow
+	 * @throws Exception
+	 */
 	public void trowResize(int rowsize, ArrayList<GTrow> extraCellTrow) throws Exception {
 		numcells = rowsize;
 
@@ -114,6 +163,12 @@ public class GTrow{
 			cells.add(new GTcell(this,x,y, extraCellTrow.get(0).cells.get(ncells).getWidth()," "));ncells++;
 		}
 	}
+	
+	
+	/**
+	 * Marks the end of table row creation. All the cells will be resized in heights right after this,
+	 * so that all cells in a row will have uniform height.
+	 */
 	public void trEnd(){
 		resizeCells();
 		//System.out.println("trEnd()"+height);
@@ -128,6 +183,9 @@ public class GTrow{
 		}
 		return maxHeight;
 	}
+	/**
+	 * This function is called by trEnd() to resize all cells in the row.
+	 */
 	private void resizeCells(){
 		maxNLines = getMaxTrHeight();   //max(nlines)
 
@@ -135,6 +193,11 @@ public class GTrow{
 			c.cellResize(maxNLines);
 		}
 	}
+	
+	
+	/** 
+	 * This method will format the data to be in text art form.
+	 */
 	public String toString(){
 		StringBuffer tmp = new StringBuffer(50);
 		Formatter fmt=new Formatter();
@@ -147,6 +210,10 @@ public class GTrow{
 		}tmp.append(fmt.toString());fmt.flush();fmt.close();
 		return tmp.toString();
 	}
+	/**
+	 * This can be used to draw upper and lower borders to a text based table representation.
+	 * @return
+	 */
 	public String drawSeparator(){
 		StringBuffer tmp = new StringBuffer(50);
 		Formatter fmt=new Formatter();
@@ -163,7 +230,12 @@ public class GTrow{
 		return tmp.toString();
 	}
 
-	public void setHeight(int height) { //this is maintained by Tcell class after trEnd on the basis of max(nlines)
+	/**
+	 * This is maintained by GTcell class after trEnd on the basis of max(nlines). This height information
+	 * will essentially be used at the end of table row creation to uniformly set all cell's heights.J
+	 * @param height
+	 */
+	public void setHeight(int height) { //this is maintained by GTcell class after trEnd on the basis of max(nlines)
 		this.height = height;
 	}
 
