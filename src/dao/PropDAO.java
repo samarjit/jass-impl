355367e1656320b290971c8e28c2f4c434106dcd
samarjit.samanta <samarjit.samanta@d445284c-bc84-11de-ad49-1f49b0b801a7>
2009-10-20 07:11:48 +0000

    added comments

--- src/dao/PropDAO.java
+++ src/dao/PropDAO.java
@@ -1,6 +1,11 @@
 package dao;
 import java.util.Collection;
 
+/**
+ * This interface defined the basic activities that must be implemented by all DAO objects
+ * @author Samarjit Samanta
+ * @param <T> T is the template class of corresponding DTO
+ */
 public interface PropDAO<T> {
 	 T select(T dt)  throws Exception;
 	void update(T dt) throws Exception;
--- src/dto/PropDTO.java
+++ src/dto/PropDTO.java
@@ -1,5 +1,12 @@
 package dto;
 
+/**
+ * This interface defines a generic DTO interface with one mandatory field specified
+ * Id. This field is basically the primary key using which all records can be uniquely 
+ * identified.
+ * @author Samarjit
+ * @version 1.0
+ */
 public interface PropDTO {
 
 	int getId();
