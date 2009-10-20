edf7f8ae4bc16bc88d49f77038f703651f2c678e
samarjit.samanta <samarjit.samanta@d445284c-bc84-11de-ad49-1f49b0b801a7>
2009-10-20 07:11:48 +0000

    added comments

--- trunk/src/dto/JobAdvertDTO.java
+++ trunk/src/dto/JobAdvertDTO.java
@@ -2,6 +2,12 @@ package dto;
 
 
 
+/**
+ * This class contains the entity JobAdvert.
+ * @author Samarjit Samanta
+ * @version 1.0
+ *
+ */
 public class JobAdvertDTO implements PropDTO{
 	
 	private int Id=0;
@@ -20,92 +26,201 @@ public class JobAdvertDTO implements PropDTO{
 	private String advertizerref;
 	
 
+	/**
+	 * gets company description
+	 * @return company description
+	 */
 	public String getCmpnyDesc() {
 		return cmpnyDesc;
 	}
+	/**
+	 * sets company description
+	 * @param cmpnyDesc
+	 */
 	public void setCmpnyDesc(String cmpnyDesc) {
 		this.cmpnyDesc = cmpnyDesc;
 	}
+	/**
+	 * gets the Id of the record
+	 * @see dto.PropDTO#getId()
+	 */
 	public int getId() {
 		return Id;
 	}
 
+	/** 
+	 * sets the Id of the record
+	 */
 	public void setId(int id) {
 		this.Id = id;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString(){
 		return "\n#id:"+Id+"|jobrefcode:"+jobrefcode+"| jobtitle:"+jobtitle+"|cmpnyDesc:"+cmpnyDesc+"| advertizerref:"+advertizerref;
 	}
+	/**
+	 * gets job reference code
+	 * @return job reference code
+	 */
 	public String getJobrefcode() {
 		return jobrefcode;
 	}
+	/**
+	 * sets job reference code
+	 * @param jobrefcode
+	 */
 	public void setJobrefcode(String jobrefcode) {
 		this.jobrefcode = jobrefcode;
 	}
+	/**
+	 * gets company name
+	 * @return company name
+	 */
 	public String getCmpname() {
 		return cmpname;
 	}
+	/**
+	 * sets compare
+	 * @param cmpname
+	 */
 	public void setCmpname(String cmpname) {
 		this.cmpname = cmpname;
 	}
+	/**
+	 * gets department
+	 * @return department
+	 */
 	public String getDepartment() {
 		return department;
 	}
+	/**
+	 * sets department
+	 * @param department
+	 */
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+	/**
+	 * gets job title
+	 * @return job title
+	 */
 	public String getJobtitle() {
 		return jobtitle;
 	}
+	/**
+	 * set job title
+	 * @param jobtitle
+	 */
 	public void setJobtitle(String jobtitle) {
 		this.jobtitle = jobtitle;
 	}
+	/**
+	 * get Jod description
+	 * @return job description
+	 */
 	public String getJobdesc() {
 		return jobdesc;
 	}
+	/**
+	 * sets job description
+	 * @param jobdesc
+	 */
 	public void setJobdesc(String jobdesc) {
 		this.jobdesc = jobdesc;
 	}
+	/**
+	 * get tech skills
+	 * @return tech skills
+	 */
 	public String getTechskills() {
 		return techskills;
 	}
+	/**
+	 * set tech skills
+	 * @param techskills
+	 */
 	public void setTechskills(String techskills) {
 		this.techskills = techskills;
 	}
+	/**
+	 * get management skills
+	 * @return management skills
+	 */
 	public String getMgmtskills() {
 		return mgmtskills;
 	}
+	/**
+	 * sets management skills
+	 * @param mgmtskills
+	 */
 	public void setMgmtskills(String mgmtskills) {
 		this.mgmtskills = mgmtskills;
 	}
+	/**
+	 * @return no of years experience
+	 */
 	public String getNoyrexp() {
 		return noyrexp;
 	}
+	/**
+	 * sets number of years experience
+	 * @param noyrexp
+	 */
 	public void setNoyrexp(String noyrexp) {
 		this.noyrexp = noyrexp;
 	}
+	/**
+	 * get salary range
+	 * @return salary range
+	 */
 	public String getSalaryrange() {
 		return salaryrange;
 	}
+	/**
+	 * sets advertiser
+	 * @param salaryrange
+	 */
 	public void setSalaryrange(String salaryrange) {
 		this.salaryrange = salaryrange;
 	}
 	public String getStartdate() {
 		return startdate;
 	}
+	/**
+	 * sets Start Date
+	 * @param startdate
+	 */
 	public void setStartdate(String startdate) {
 		this.startdate = startdate;
 	}
+	/**
+	 * gets location
+	 * @return location
+	 */
 	public String getLocation() {
 		return location;
 	}
+	/**
+	 * sets location
+	 * @param location
+	 */
 	public void setLocation(String location) {
 		this.location = location;
 	}
+	/**
+	 * gets advertiser
+	 * @return advertiser
+	 */
 	public String getAdvertizerref() {
 		return advertizerref;
 	}
+	/**
+	 * sets advertiser
+	 * @param advertizerref
+	 */
 	public void setAdvertizerref(String advertizerref) {
 		this.advertizerref = advertizerref;
 	}
