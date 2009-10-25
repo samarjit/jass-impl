b171d532245395dccd5b0b20c1b3dd7e4d0dabae
satishpops <satishpops@d445284c-bc84-11de-ad49-1f49b0b801a7>
2009-10-25 05:39:03 +0000

    company DTO completed

--- src/dto/CompanyDTO.java
+++ src/dto/CompanyDTO.java
@@ -1,5 +1,35 @@
 package dto;
 
 public class CompanyDTO {
+	
+	private int id;
+	private String CompanyID;
+	private String CompanyName;
+	
+	
+	public String getCompanyID() {
+		return CompanyID;
+	}
+
+	public void setCompanyID(String companyID) {
+		CompanyID = companyID;
+	}
+
+	public String getCompanyName() {
+		return CompanyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		CompanyName = companyName;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 
 }
