5d3f569bd626531b4c10dc0787a366d984b0d841
justinjose12002 <justinjose12002@d445284c-bc84-11de-ad49-1f49b0b801a7>
2009-10-27 19:04:24 +0000

    removed responsedto

--- trunk/test/suite/AllTests.java
+++ trunk/test/suite/AllTests.java
@@ -24,8 +24,7 @@ public class AllTests extends TestCase {
 	 * @return JASS test suite
 	 */
 	public static Test suite(){
-		TestSuite suite = new TestSuite("Test for JASS package");
-		suite.addTestSuite(ResponseDTOTest.class);
+		TestSuite suite = new TestSuite("Test for JASS package");		
 		suite.addTestSuite(ResponseDAOTest.class);
 		suite.addTestSuite(JobAdvertDTOTest.class);
 		suite.addTestSuite(JobAdvertDAOTest.class);
